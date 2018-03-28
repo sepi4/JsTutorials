f33be8a746639dadb5483f49e9904b7a4c64027f
Sergei Polishsuk <sepolish@student.jyu.fi>
2018-03-28 15:31:05 +0300

    making gameOfLife

---
+++ gameOfLife/sketch.js
@@ -0,0 +1,108 @@
+//  n < 2 => dies
+// (n == 2 || n == 3) => live
+//  n > 3 => dies
+// n == 3 => born
+
+class Cell {
+  constructor(i, j) {
+    this.i = i;
+    this.j = j;
+    this.value;
+  }
+}
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 20;
+let frames = 1;
+let looping = false;
+
+function setup() {
+  createCanvas(600, 400);
+  cols = width / resolution;
+  rows = height / resolution
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j, floor(random(2)));
+    }
+  }
+  console.table(grid);
+  frameRate(frames);
+  noLoop();
+  let startBtn = document.getElementById("start-btn");
+  startBtn.addEventListener("click", function() {
+    if (looping) {
+      looping = !looping;
+      startBtn.textContent = "Start";
+      noLoop();
+    } else {
+      looping = !looping;
+      startBtn.textContent = "Pause";
+      loop();
+    }
+  });
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j].value == 1) {
+        fill(255);
+        stroke(0);
+        rect(x, y, resolution-1, resolution-1);
+      }
+    }
+  }
+
+
+  let next = make2DArray(cols, rows);
+
+  // compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state === 0 && neighbors === 3) {
+        next[i][j].value = 1;
+      } else if (state === 1 && neighbors < 2 || neighbors > 3) {
+        next[i][j].value = 0;
+      } else {
+        next[i][j] = state;
+      }
+    
+    }
+  }
+
+  grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
+
