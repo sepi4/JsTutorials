4fc0092c9ed0f310a1040e6df0230e3cf2aeb7b9
Sergei Polishsuk <sepolish@student.jyu.fi>
2018-03-29 23:44:22 +0300

    gameOfLife working

--- gameOfLife/sketch.js
+++ gameOfLife/sketch.js
@@ -3,12 +3,68 @@
 //  n > 3 => dies
 // n == 3 => born
 
+let grid;
+let cols;
+let rows;
+let resolution = 50;
+let frames = 1;
+let looping = false;
+
 class Cell {
   constructor(i, j) {
     this.i = i;
     this.j = j;
-    this.value;
+    this.x = i * resolution;
+    this.y = j * resolution;
+    this.state;
+    this.next = floor(random(2));
   }
+
+  static kirjoita() {
+    console.log('kirjoita');
+  }
+
+  drawRect(color) {
+    fill(color);
+    stroke(0);
+    rect(this.x, this.y, resolution - 1, resolution - 1);
+  }
+}
+
+
+function setup() {
+  createCanvas(600, 400);
+  cols = width / resolution;
+  rows = height / resolution
+  grid = make2DArray(cols, rows);
+
+  //adding Cells
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j);
+    }
+  }
+
+  frameRate(frames);
+  noLoop();
+
+  // event listeners to buttons
+  let startBtn = document.getElementById("start-btn");
+  startBtn.addEventListener("click", function() {
+    if (looping) {
+      startBtn.textContent = "Start";
+      noLoop();
+    } else {
+      startBtn.textContent = "Pause";
+      loop();
+    }
+    looping = !looping;
+  });
+
+  let oneLoopBtn = document.getElementById("one-loop");
+  oneLoopBtn.addEventListener("click", function() {
+    oneTurn();
+  });
 }
 
 function make2DArray(cols, rows) {
@@ -19,78 +75,74 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
-let grid;
-let cols;
-let rows;
-let resolution = 20;
-let frames = 1;
-let looping = false;
-
-function setup() {
-  createCanvas(600, 400);
-  cols = width / resolution;
-  rows = height / resolution
-  grid = make2DArray(cols, rows);
+function mousePressed() {
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      // grid[i][j] = new Cell(i, j, floor(random(2)));
-      grid[i][j] = floor(random(2));
+      let g = grid[i][j];
+      if (mouseX > g.x && mouseX < g.x + resolution && mouseY > g.y && mouseY < g.y + resolution) {
+        console.log(g);
+        g.state = g.state === 1 ? 0 : 1;
+      }
     }
   }
-  console.table(grid);
-  frameRate(frames);
-  noLoop();
-  let startBtn = document.getElementById("start-btn");
-  startBtn.addEventListener("click", function() {
-    if (looping) {
-      looping = !looping;
-      startBtn.textContent = "Start";
-      noLoop();
-    } else {
-      looping = !looping;
-      startBtn.textContent = "Pause";
-      loop();
-    }
-  });
+  computeNexts();
+  drawGrid();
 }
 
-function draw() {
-  background(0);
+function drawGrid() {
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-      if (grid[i][j] === 1) {
-        fill(255);
-        stroke(0);
-        rect(x, y, resolution-1, resolution-1);
+      let g = grid[i][j];
+      if (g.state === 1) {
+        g.drawRect(255);
+      } else {
+        g.drawRect(100);
+      }
+    }
+  }
+}
+
+function oneTurn() {
+  // drawing loops
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let g = grid[i][j];
+      g.state = g.next;
+      if (g.state === 1) {
+        g.drawRect(255);
+      } else {
+        g.drawRect(100);
       }
     }
   }
 
+  //computing nexts
+  computeNexts();
+}
 
-  let next = make2DArray(cols, rows);
-
-  // compute next based on grid
+function computeNexts() {
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      let state = grid[i][j];
+      let state = grid[i][j].state;
 
       let sum = 0;
       let neighbors = countNeighbors(grid, i, j);
 
       if (state === 0 && neighbors === 3) {
-        next[i][j] = 1;
-      } else if (state === 1 && neighbors < 2 || neighbors > 3) {
-        next[i][j] = 0;
+        grid[i][j].next = 1;
+      } else if (state === 1 && neighbors < 2 || 
+        neighbors > 3) {
+        grid[i][j].next = 0;
       } else {
-        next[i][j] = state;
+        grid[i][j].next = state;
       }
-    
     }
   }
+}
 
-  grid = next;
+function draw() {
+  background(0);
+  oneTurn();
 }
 
 function countNeighbors(grid, x, y) {
@@ -100,10 +152,10 @@ function countNeighbors(grid, x, y) {
       let col = (x + i + cols) % cols;
       let row = (y + j + rows) % rows;
 
-      sum += grid[col][row];
+      sum += grid[col][row].state;
     }
   }
-  sum -= grid[x][y];
+  sum -= grid[x][y].state;
   return sum;
 }
 
